a49c7b07296f99e6bcb3d84ab328cc9f5ab1df13
alzamo12 <rafiqulislam4969@gmail.com>
2025-12-16 20:22:13 +0600

    make a delete task api with proper verification

--- index.js
+++ index.js
@@ -540,6 +540,18 @@ async function run() {
                 console.log(err)
                 res.status(500).send({ message: "Internal server error" })
             }
+        });
+
+        app.delete("/task/:id", verifyToken, async (req, res) => {
+            try {
+                const { id } = req.params;
+                const query = { _id: new ObjectId(id), userEmail: req.user.email };
+                const result = await tasksCollection.deleteOne(query);
+                res.send(result)
+            } catch (err) {
+                console.error(err)
+                res.status(500).send({ message: "Internal server error" })
+            }
         })
 
         // classesCollection.deleteMany()
